47519667e00b7287d5f5305ba7647287797e802b
Tajinder Sharma <sharma.tajinder01@gmail.com>
2025-05-15 14:33:01 -0700

    change some fonts to bold and italics

--- solucky-react-app/src/components/Collabs.jsx
+++ solucky-react-app/src/components/Collabs.jsx
@@ -6,30 +6,32 @@ const Collabs = () =>{
     return(
         <div className="collabs-page">
             <div className="top-section">
-                <div className="content-container-grid">
-                    <div className="create-image-container">
-                        <img className="svg-image" alt="create-with-solucky" src="../Assets/Collabs/create-image.svg"></img>
-                    </div>
-                    <div className="collab-paragraph-container">
-                        <div className="magazine-para-text">
-                            A collaboration with solucky creates a seamless partnership 
-                            between your business & our creative brand. Simply send us 
-                            your products, & our team of experts including models, 
-                            production crew, & creative directors will take care of the
-                            rest. After the shoot(s), we’ll return your products along
-                            with high-quality content that you can freely use across 
-                            your social media platforms.
+                <div className="content-container-div">
+                    <div className="top-content-wrapper">
+                        <div className="create-image-container">
+                            <img className="svg-image" alt="create-with-solucky" src="../Assets/Collabs/create-image.svg"></img>
                         </div>
-                        <div className="magazine-para-text">
-                            Additionally, this content will be featured on our social 
-                            media channels & website, expanding your reach & boosting 
-                            engagement. All that we ask in return is that you credit our 
-                            brand & team, along with our collaboration title. If desired, 
-                            we’re happy to collaborate on the creative direction, ensuring 
-                            a vision both parties love.
-                        </div>
-                        <div className="magazine-para-text">
-                            Email solucky.contact@gmail.com to collab with us!
+                        <div className="collab-paragraph-container">
+                            <div className="magazine-para-text">
+                                A <b>collaboration</b> with <i>solucky</i> creates a <b>seamless partnership </b>
+                                between your business & our creative brand. Simply send us 
+                                your products, & our team of experts including models, 
+                                production crew, & creative directors will take care of the
+                                rest. After the shoot(s), we’ll return your products along
+                                with high-quality content that you can freely use across 
+                                your social media platforms.
+                            </div>
+                            <div className="magazine-para-text">
+                                Additionally, this content will be featured on our social 
+                                media channels & website, expanding your reach & boosting 
+                                engagement. <b>All that we ask</b> in return is that you credit our 
+                                brand & team, along with our collaboration title. If desired, 
+                                we’re happy to collaborate on the creative direction, ensuring 
+                                <b> a vision both parties love</b>.
+                            </div>
+                            <div className="magazine-para-text">
+                                Email solucky.contact@gmail.com to collab with us!
+                            </div>
                         </div>
                     </div>
                     <div className="bracelet-svg-container">
