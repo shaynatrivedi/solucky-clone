3fe29027e3c3fb7d583b64b955184ab24e4bc823
Tajinder Sharma <sharma.tajinder01@gmail.com>
2025-05-16 02:28:09 -0700

    use singular svg instead of seperate captions

--- solucky-react-app/src/components/Shop.jsx
+++ solucky-react-app/src/components/Shop.jsx
@@ -12,12 +12,12 @@ const Shop = () => {
           const atEnd = scrollLeft + clientWidth >= scrollWidth;
 
           if (!atEnd) {
-            scrollRef.current.scrollLeft += 290;
+            scrollRef.current.scrollLeft += 90;
           } else {
             clearInterval(scrollInterval);
           }
         }
-      }, 2000);
+      }, 200);
 
       return () => clearInterval(scrollInterval);
     }, []);
@@ -25,7 +25,7 @@ const Shop = () => {
 
     return (
         <div className="shop-page">
-          <div className="top-grid">
+          <div className="top-shop-div">
             <div className="shop-link-div">
               <div className="shop-link-title">solucky wardrobe</div>
               <a href="https://www.instagram.com/stories/highlights/17897013209879517/?hl=en">
@@ -36,20 +36,8 @@ const Shop = () => {
               </a>   
             </div>
 
-            <div className="label-1">
-              <p className="cutout-captions cap1">WORTHINGTON velvet long sleeve $7</p>
-            </div>
-
-            <div className="label-2" >
-              <p className="cutout-captions cap2">AMERICAN apparel plaid skirt $8</p>
-            </div>
-
-            <div className="cutout-image">
-              <img src='/Assets/Shop/full-cutout.png' alt='outfit-cutout'></img>
-            </div>
-
-            <div className="label-3">
-              <p className="cutout-captions cap3">ALDO boots $12</p>
+            <div className="cutout-image-wrapper">
+              <img src='/Assets/Shop/top-cutout.svg' alt='outfit-cutout'></img>
             </div>
 
           </div>
