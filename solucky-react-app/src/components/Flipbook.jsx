2bd0baa212b49b45b59484f764797aa92afbe9ab
Taj Sharma <sharma.tajinder01@gmail.com>
2025-05-17 02:52:28 -0700

    add flipbook to page needs fixing

--- solucky-react-app/src/components/Flipbook.jsx
+++ solucky-react-app/src/components/Flipbook.jsx
@@ -1,43 +1,39 @@
 import React from "react";
-import HTMLFlipBook from "react-pageflip";
+import FlipPage from "react-flip-page";
 import "./Flipbook.css";
 
-const MagazineFlipbook = () => {
-  const pages = [
-    "/Assets/Flipbook/cover1.png",
-    "/Assets/Flipbook/cover2.png",
-    "/Assets/Flipbook/cover3.png",
-    "/Assets/Flipbook/cover4.png",
-    "/Assets/Flipbook/cover5.png",
-    "/Assets/Flipbook/cover6.png",
-    "/Assets/Flipbook/cover7.png",
-    "/Assets/Flipbook/cover8.png",
-    "/Assets/Flipbook/cover9.png",
-    "/Assets/Flipbook/cover10.png",
-  ];
+const pages = [
+  "/Assets/Flipbook/cover1.png",
+  "/Assets/Flipbook/cover2.png",
+  "/Assets/Flipbook/cover3.png",
+  "/Assets/Flipbook/cover4.png",
+  "/Assets/Flipbook/cover5.png",
+  "/Assets/Flipbook/cover6.png",
+  "/Assets/Flipbook/cover7.png",
+  "/Assets/Flipbook/cover8.png",
+  "/Assets/Flipbook/cover9.png",
+  "/Assets/Flipbook/cover10.png",
+];
 
+const Flipbook = () => {
   return (
     <div className="flipbook-wrapper">
-      <HTMLFlipBook
-        width={550}
-        height={700}
-        size="stretch"
-        minWidth={315}
-        maxWidth={1200}
-        minHeight={400}
-        maxHeight={1000}
-        showCover={true}
-        mobileScrollSupport={true}
-        className="flipbook"
+      <FlipPage
+        orientation="horizontal"
+        uncutPages
+        showHint
+        animationDuration="800"
+        responsive
+        
       >
         {pages.map((src, i) => (
-          <div key={i} className="page">
+          <article key={i} className="page">
             <img src={src} alt={`page-${i}`} />
-          </div>
+          </article>
         ))}
-      </HTMLFlipBook>
+      </FlipPage>
     </div>
   );
 };
 
-export default MagazineFlipbook;
+export default Flipbook;
