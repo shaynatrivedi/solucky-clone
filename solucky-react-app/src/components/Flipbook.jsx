c8f1e520ed0c51c9d3a04c14b79c29ea8d3d266b
Taj Sharma <sharma.tajinder01@gmail.com>
2025-05-21 16:24:12 -0700

    remove old npm flipbook

--- solucky-react-app/src/components/Flipbook.jsx
+++ solucky-react-app/src/components/Flipbook.jsx
@@ -14,11 +14,18 @@ const Flipbook = () => {
     maxShadowOpacity={0.5}
     drawShadow={true}
     showCover={true}
+    size='fixed'
     >
-      <div className="demoPage">Page 1</div>
-      <div className="demoPage">Page 2</div>
-      <div className="demoPage">Page 3</div>
-      <div className="demoPage">Page 4</div>
+      <div className="page-template cover">1</div>
+      <div className="page-template page1">2</div>
+      <div className="page-template page2">3</div>
+      <div className="page-template page3">45</div>
+      <div className="page-template page4"></div>
+      <div className="page-template page5"></div>
+      <div className="page-template page6"></div>
+      <div className="page-template page7"></div>
+      <div className="page-template page8"></div>
+      <div className="page-template page9"></div>
     </HTMLFlipBook>
   );
 };
